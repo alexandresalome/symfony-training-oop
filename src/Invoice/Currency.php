b26c4bf90ad29b022bb9c2ae6bc8b97373dbc113
Alexandre Salomé <alexandre.salome@blackfire.io>
2021-04-07 15:38:17 +0200

    Add Currency

---
+++ src/Invoice/Currency.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Invoice;
+
+class Currency
+{
+    private const ALLOWED_CURRENCIES = ['EUR', 'USD'];
+
+    private string $currency;
+
+    public function __construct(string $currency)
+    {
+        if (!in_array($currency, self::ALLOWED_CURRENCIES, true)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Currency is "%s", expected one of %s.',
+                $currency,
+                implode(', ', self::ALLOWED_CURRENCIES)
+            ));
+        }
+        $this->currency = $currency;
+    }
+
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+}
